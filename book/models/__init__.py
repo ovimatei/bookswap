edeffc4e9abe3ec5db6e334d04fd15b63d03828b
ovimatei <ovidiumatei90@gmail.com>
2020-11-04 12:15:04 +0200

    Set order in models init, update requirements

---
+++ book/models/__init__.py
@@ -0,0 +1,5 @@
+from .author import Author
+from .category import Category
+from .book import Book
+from .user_book import UserBook
+from .request import Request
--- book/models/request.py
+++ book/models/request.py
@@ -7,4 +7,4 @@ class Request(models.Model):
     status = models.CharField(max_length=64)
     requested_book = models.ForeignKey(UserBook, on_delete=models.CASCADE)
     offered_book = models.ForeignKey(UserBook, on_delete=models.CASCADE)
-    request_date = models.DateField()
+    request_date = models.DateField(auto_now=True)
